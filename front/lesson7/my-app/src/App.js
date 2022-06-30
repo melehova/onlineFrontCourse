03fd6e1916ddde0748a378a654abcc9ea41a246f
vmelekhova <vmelekhova@stud.khpi.ucode-connect.study>
2022-06-30 20:42:36 +0300

    added lesson9 frontend

--- front/lesson7/my-app/src/App.js
+++ front/lesson7/my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import MainBlock from '../../../lesson7/my-app/src/components/contentBlocks/mainBlock';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
@@ -13,22 +14,21 @@ function App() {
   return (
     <div className="App">
       < Header />
-      < MainBlock clickButtonFunc={clickButtonFunc} number={number} display={true} color='red' content='beeeebra'/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      < MainBlock clickButtonFunc={clickButtonFunc} number={number} display={true} color='red' content='beeeebra' />
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/hello" component={(
+            < MainBlock clickButtonFunc={clickButtonFunc} number={number} display={true} color='red' content='beeeebra' />
+          )} />
+          <Route path="/hllo-world" component={(
+            < MainBlock clickButtonFunc={clickButtonFunc} number={number} display={true} color='red' content='Helloworld' />
+          )} />
+        </Routes>
+
+      </BrowserRouter>
+    </div >
   );
 }
 
--- front/lesson7/my-app/src/components/contentBlocks/mainBlock/index.js
+++ front/lesson7/my-app/src/components/contentBlocks/mainBlock/index.js
@@ -1,10 +1,75 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './mainBlock.css'
 
 const MainBlock = ({ clickButtonFunc, content, display, color }) => {
 
+    const blog = [
+        {
+            key: 0,
+            title: 'AAA',
+            author: 'Valentin',
+            date: '03.03.03'
+        },
+        {
+            key: 1,
+            title: 'vvv',
+            author: 'Valentin',
+            date: '03.03.03'
+        },
+        {
+            key: 2,
+            title: 'AdfdfAA',
+            author: 'dfsdf',
+            date: '03.03.03'
+        },
+        {
+            key: 3,
+            title: 'efsef',
+            author: 'Valefdgdfgfgntin',
+            date: '03.03.03'
+        }
+    ]
 
-    const [ number, setNumber ] = useState(469)
+    const addPost = {
+        title: 'efsef',
+        author: 'Valefdgdfgfgntin',
+        date: '03.03.03'
+    }
+
+    const [number, setNumber] = useState(blog.length)
+    const [posts, setPosts] = useState(blog)
+
+
+    // const divRef = document.getElementById('beb').createRef()
+
+    useEffect(() => {
+        const newBlog = [...blog]
+        newBlog[2].title = document.getElementById('textInput').value
+        // if (names.includes('oleh')) {
+        //     newBlog[2].title = 'ooo'
+        // }
+        setPosts(newBlog)
+
+        document.getElementById('beb').innerText = document.getElementById('textInput').value
+        // divRef.ariaCurrent.innerText = document.getElementById('textInput').value
+        // setPosts([...posts, addPost]) 
+        // blog.push(addPost)
+    }, [number])
+
+    /// при любом изменении
+    // useEffect(() => {
+    //     setPosts([...posts, addPost])
+    //     blog.push(addPost)
+    // })
+
+
+    /// только при первоначайной загрузке страницы
+
+    // useEffect(() => {
+    //     setPosts([...posts, addPost])
+    //     blog.push(addPost)
+    // }, [])  
+
     if (!display) {
         return ''
     }
@@ -12,20 +77,29 @@ const MainBlock = ({ clickButtonFunc, content, display, color }) => {
         setNumber(number + 1)
     }
     const minusNumber = () => {
-        setNumber(Math.max(number - 1 , 0))
-        // setNumber(Math.round(Math.max(number - 1 , 0) / 100) * 100)
-
+        setNumber(Math.max(number - 1, 0))
     }
+
     return (
         <div className={`main-block ${color = 'red' ? 'red' : ''}`}>
-                <div className='container' style={{ borderBottom: '5px solid blue', borderTop: '2px dashed yellow'}}>
-                    Main block {content} {number}
-                    <div onClick={plusNumber}>+</div>
-                    <div>{number}</div>
-                    <div onClick={minusNumber}>-</div>
-                    <button onClick={clickButtonFunc}>aaaaaa</button>
-                </div>
+            <div className='container' style={{ borderBottom: '5px solid blue', borderTop: '2px dashed yellow' }}>
+                Main block {content} {number}
+                <div onClick={plusNumber}>+</div>
+                <div>{number}</div>
+                <div onClick={minusNumber}>-</div>
+                <button onClick={clickButtonFunc}>aaaaaa</button>
             </div>
+            <input type="text" id="textInput"></input>
+            {/* <div id='beb' ref={divRef}></div> */}
+            <div id='beb'></div>
+
+
+            {
+                blog.map(({ key, title, author }) => {
+                    return <div key={key}>{title} - {author}</div>
+                })
+            }
+        </div>
     )
 }
 
